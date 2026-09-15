ffc7c66ea354559dd1fa308dce839b09010f3eda
Jeff Thomas III <Masterstudent@duck.com>
2026-09-15 11:23:57 -0700

    Bump PWA cache after standalone shell rollout

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'epochlex-pwa-20260915-2';
+const CACHE_VERSION = 'epochlex-pwa-20260915-3';
 const CORE_CACHE = `${CACHE_VERSION}-core`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 
